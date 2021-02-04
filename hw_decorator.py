b44412721ea93953221f517ad4cb32e01fbac11b
VodopianovRoman <77894414+VodopianovRoman@users.noreply.github.com>
2021-02-04 13:45:17 +0200

    maybe done 4 exercise

--- hw_decorator.py
+++ hw_decorator.py
@@ -51,7 +51,7 @@ import functools
 #     print((int_arg * 100) / 20)
 
 
-number_function(2)
+# number_function(2)
 
 # ЗАДАЧА-3
 # Написать декоратор который будет кешировать значения аргументов и результаты работы
@@ -63,3 +63,58 @@ number_function(2)
 # Если значение берется из переменной cache, вывести сообщение
 # «Used cache with counter = {}» и
 # количество раз обращений в cache.
+
+# def decorator_fib(func):
+#     cache = {}
+#     @functools.wraps(func)
+#     def wrap(*args):
+#         if args in cache:
+#             wrap.ccalls += 1
+#             print(f'Used cache with counter = {fib.ccalls}')
+#             return cache[args]
+#         else:
+#             cache[args] = func(*args)
+#             wrap.ncalls += 1
+#             print(f'Function executed with counter = {fib.ncalls}, function result = {func(*args)}')
+#             return cache[args]
+#
+#
+#     wrap.ccalls = 0
+#     wrap.ncalls = 0
+#     return wrap
+#
+#
+# @decorator_fib
+# def fib(n):
+#     if n < 2:
+#         return n
+#     return fib(n-2) + fib(n-1)
+#
+# print('fib(20) =', fib(20))
+
+
+
+# def decorator_multi(func):
+#     cache = {}
+#     @functools.wraps(func)
+#     def wrap(*args):
+#         if args in cache:
+#             wrap.ccalls += 1
+#             print(f'Used cache with counter = {wrap.ccalls}')
+#             return cache[args]
+#         else:
+#             wrap.ncalls += 1
+#             cache[args] = func(*args)
+#             print(f'Function executed with counter = {wrap.ncalls}, function result = {func(*args)}')
+#             return cache[args]
+#
+#     wrap.ccalls = 0
+#     wrap.ncalls = 0
+#     return wrap
+#
+#
+# @decorator_multi
+# def multi_bulti(int_n):
+#     return int_n ** int_n
+#
+# multi_bulti(5)
