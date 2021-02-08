276a9ac761f69ea71f4b1c771548ac45c337de5a
VodopianovRoman <77894414+VodopianovRoman@users.noreply.github.com>
2021-02-08 12:08:41 +0200

    Did my job. Unfortunately, apparently I did not quite understand task # 2. In task number 3, I tried to solve it in different ways, but nowhere did I achieve the desired result. In principle, it seems to work, but not as we would like.

--- hw_decorator.py
+++ hw_decorator.py
@@ -49,9 +49,9 @@ import functools
 # @decorator_check_for_number
 # def number_function(int_arg):
 #     print((int_arg * 100) / 20)
-
-
-# number_function(2)
+#
+#
+# number_function(20)
 
 # ЗАДАЧА-3
 # Написать декоратор который будет кешировать значения аргументов и результаты работы
@@ -63,36 +63,6 @@ import functools
 # Если значение берется из переменной cache, вывести сообщение
 # «Used cache with counter = {}» и
 # количество раз обращений в cache.
-
-# def decorator_fib(func):
-#     cache = {}
-#     @functools.wraps(func)
-#     def wrap(*args):
-#         if args in cache:
-#             wrap.ccalls += 1
-#             print(f'Used cache with counter = {fib.ccalls}')
-#             return cache[args]
-#         else:
-#             cache[args] = func(*args)
-#             wrap.ncalls += 1
-#             print(f'Function executed with counter = {fib.ncalls}, function result = {func(*args)}')
-#             return cache[args]
-#
-#
-#     wrap.ccalls = 0
-#     wrap.ncalls = 0
-#     return wrap
-#
-#
-# @decorator_fib
-# def fib(n):
-#     if n < 2:
-#         return n
-#     return fib(n-2) + fib(n-1)
-#
-# print('fib(20) =', fib(20))
-
-
 
 # def decorator_multi(func):
 #     cache = {}
@@ -117,4 +87,103 @@ import functools
 # def multi_bulti(int_n):
 #     return int_n ** int_n
 #
-# multi_bulti(5)
+# print(multi_bulti(5))
+
+
+
+# def decorator_fib(func):
+#     cache = {}
+#     ncalls = 0
+#     ccalls = 0
+#     @functools.wraps(func)
+#     def wrap(*args):
+#         nonlocal ccalls
+#         nonlocal ncalls
+#         if args in cache:
+#             ccalls += 1
+#             print(f'Used cache with counter = {ccalls}')
+#             return cache[args]
+#         else:
+#             cache[args] = func(*args)
+#             ncalls += 1
+#             print(f'Function executed with counter = {ncalls}, function result = {func(*args)}')
+#             return cache[args]
+#
+#     return wrap
+#
+#
+# @decorator_fib
+# def fib(n):
+#     if n < 2:
+#         return n
+#     return fib(n-2) + fib(n-1)
+#
+# print('fib(20) =', fib(20))
+
+
+# def decorator_cash(func):
+#     cache = {}
+#     @functools.wraps(func)
+#     def wrapper(*args):
+#         if args in cache:
+#             wrapper.ccalls += 1
+#             return cache[args]
+#         else:
+#             wrapper.ncalls += 1
+#             cache[args] = func(*args)
+#             return cache[args]
+#
+#     wrapper.ccals = 0
+#     wrapper.ncals = 0
+#     return wrapper
+#
+# def decorator_print_res(func):
+#     @functools.wraps(func)
+#     def wrapper(*args):
+#         print(f'Used cache with counter = {func.ccalls}')
+#         print(f'Function executed with counter = {func.ncalls}, function result = {func(*args)}')
+#     return wrapper
+#
+# @decorator_cash
+# @decorator_print_res
+# def fib(n):
+#     if n < 2:
+#         return n
+#     return fib(n-2) + fib(n-1)
+#
+# print('fib(20) =', fib(20))
+
+
+
+
+
+
+
+# ======================Практика=========================================================
+
+# import time
+#
+# def decorator_time_name_result(func):
+#     """The decorator shows the runtime and the name and result of the func being received and
+#     writes the data to file log.txt"""
+#     @functools.wraps(func)
+#
+#     def wrapper(*args, **kwargs):
+#         start = time.time()
+#         func(*args, **kwargs)
+#         end = time.time()
+#         print(f'Результат - {func(*args, **kwargs)}')
+#         print(f'Имя функции - {func.__name__}')
+#         print(f'Время работы - {end-start}')
+#         all_res = f'Результат - {func(*args, **kwargs)} Имя функции - {func.__name__} Время работы - {end-start}'
+#         with open('log.txt', 'w', encoding='utf-8') as file:
+#             file.write(all_res)
+#
+#     return wrapper
+#
+# @decorator_time_name_result
+# def my_func(a):
+#     """Return 'a'. 'a' is int"""
+#     return a
+#
+# my_func(10)
