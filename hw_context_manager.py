0bd31551fa22db417b33672e8ff3d6c7a6b7b5b3
VodopianovRoman <77894414+VodopianovRoman@users.noreply.github.com>
2021-02-24 14:21:30 +0200

    Documented the code.

--- hw_context_manager.py
+++ hw_context_manager.py
@@ -9,6 +9,12 @@ import time
 from contextlib import suppress
 
 class OpenFolder:
+    """This context manager takes the path to the folder and the name of the error.
+    In the __init__ block, we save our current file location.
+    The folder is opened in the __enter__ blocks, here the error is suppressed using the construction
+try except, if an error occurs, we enter the message and remain at the current location.
+    In the __exit__ block, the saved location is closed."""
+
     def __init__(self, path, *exception):
         self.path = path
         self.exception = exception
@@ -23,6 +29,7 @@ class OpenFolder:
             os.chdir(self.path)
         except self.exception:
             print('Ooops, something went wrong')
+            os.chdir(self.saved_cwd)
 
         return self.path
 
@@ -30,7 +37,7 @@ class OpenFolder:
         """Closing the old path"""
         os.chdir(self.saved_cwd)
 #
-# with OpenFolder(r'C:\Users', FileNotFoundError):
+# with OpenFolder(r'C:\asd', FileNotFoundError):
 #     print(os.getcwd())
 
 # Задача -2
@@ -38,26 +45,36 @@ class OpenFolder:
 
 @contextlib.contextmanager
 def open_folder(path, *exception):
+    """With the help of this decorator of the context manager, we get the path to the folder and
+    go to it using the construction try/except.
+        We keep our current location.
+        In the final implementation, when invoked using visas, we use the suppres tool
+    from the contextLib.
+        Since without it, when specifying a non-existent path, the yield is swearing."""
     now_cwd = os.getcwd()
     try:
         os.chdir(path)
         yield
     except exception:
-        print('Ooops, something went wrong')
         os.chdir(now_cwd)
+        print('Ooops, something went wrong')
     finally:
         os.chdir(path)
 
-with suppress(FileNotFoundError):
-    with open_folder(r'C:\Users', FileNotFoundError) as op_fol:
-        op_fol =os.getcwd()
-        print(op_fol)
+# with suppress(FileNotFoundError):
+#     with open_folder(r'C:\asd', FileNotFoundError):
+#         print(os.getcwd())
 
 # Задача -3
 # Создать менеджер контекста который будет подсчитывать время выполнения вашей функции
 
 class CountOfFunc:
-    def __init__(self, ):
+    """With the help of this context manager, we can calculate the execution time of the
+    required function or context manager.
+        In the variable time.start, we will remember the current time.
+        In the __enter__ block, in try we start the countdown.
+        In the __exit__ block, we display the time that was spent."""
+    def __init__(self):
         self.time_start = time.time()
         # self.context_manager = context_manager
         # self.exception = exception
